ca103a7030e83da1cccdc63c729014099300435a
Nat Arrazola <NatyArrazola13@gmail.com>
2026-07-22 19:51:40 -0600

    Complete JavaScript Day 02 Level 1 Part 2

---
+++ JavaScript/30-Days-of-JavaScript/02-Day-Exercises/Level-1/04-string-validation.js
@@ -0,0 +1,18 @@
+// 20. Use trim() para eliminar cualquier espacio en blanco final al principio y al final de una cadena. Por ejemplo, '30 días de JavaScript'.
+
+let desafio = ' 30 Days Of JavaScript ';
+console.log(desafio.trim());
+
+// 21. Use el método startsWith() con la cadena 30 días de JavaScript y haga que el resultado sea verdadero.
+
+console.log(desafio.startsWith("3"));
+
+// 22. Use el método endsWith() con la cadena 30 días de JavaScript y haga que el resultado sea verdadero.
+
+console.log(desafio.endsWith("t"));
+
+
+// 23. Usa el método match() para encontrar todos los 'a' en 30 días de JavaScript.
+
+console.log(desafio.match("a"));
+
---
+++ JavaScript/30-Days-of-JavaScript/02-Day-Exercises/Level-1/05-string-combination.js
@@ -0,0 +1,9 @@
+// 24. Use concat() y fusione '30 días de' y 'JavaScript' en una sola cadena, '30 días de JavaScript'.
+
+let desafio = "30 días de ";
+console.log(desafio.concat("JavaScript"));
+
+// 25. Use el método repeat() para imprimir 30 días de JavaScript 2 veces.
+
+console.log(desafio.repeat(2));
+
