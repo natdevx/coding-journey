9390e56e49d0197e01663e4547072dbe27d97fc7
Nat Arrazola <NatyArrazola13@gmail.com>
2026-07-25 07:28:04 -0600

    Fix Day 4 Level 2 Exercise 3 v2

--- JavaScript/30-Days-of-JavaScript/04-Day-Exercises/Level-2/03-weekdays.js
+++ JavaScript/30-Days-of-JavaScript/04-Day-Exercises/Level-2/03-weekdays.js
@@ -1,4 +1,4 @@
-/* 3. Compruebe si un día es un día de fin de semana o un día laborable. Su script tomará el día como entrada.
+/* 3. Compruebe si un día es un día de fin de semana o un día laborable.
 
     ¿Qué día es hoy? Sábado
     El sábado es fin de semana.
@@ -11,15 +11,27 @@
 
     ¿Qué día es hoy? ViErNes
     El viernes es un día laborable.
-
 */
+
 let day = prompt("¿Qué día es hoy?").trim().toLowerCase();
 
-if (day === "sábado" ||day === "sabado" ||day === "domingo") {
-    console.log(`El ${day} es fin de semana.`);
-} else if (day === "lunes" ||day === "martes" ||day === "miércoles" ||day === "miercoles" ||day === "jueves" ||day === "viernes") {
-    console.log(`El ${day} es un día laborable.`);
-} else {
-    console.log(`"${day}" no es un día válido de la semana.`);
+switch (day) {
+    case "sábado":
+    case "sabado":
+    case "domingo":
+        console.log(`El ${day} es fin de semana.`);
+        break;
+
+    case "lunes":
+    case "martes":
+    case "miércoles":
+    case "miercoles":
+    case "jueves":
+    case "viernes":
+        console.log(`El ${day} es un día laborable.`);
+        break;
+
+    default:
+        console.log(`"${day}" no es un día válido de la semana.`);
 }
 
