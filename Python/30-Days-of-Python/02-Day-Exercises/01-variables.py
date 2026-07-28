7d07703d646555265658992f4d312b5c005a0ee5
Nat Arrazola <NatyArrazola13@gmail.com>
2026-07-27 21:15:06 -0600

    Add Day 2 Level 1 Exercises

---
+++ Python/30-Days-of-Python/02-Day-Exercises/01-variables.py
@@ -0,0 +1,48 @@
+# EXERCISES LEVEL 1
+# Día 2: 30 Days of Python programming.
+
+# 1. Declara una variable first_name y asígnale un valor.
+first_name = 'Nat'
+print('Nombre:',first_name)
+
+# 2. Declara una variable last_name y asígnale un valor.
+last_name = 'Arrazola'
+print('Apellido:',last_name)
+
+# 3. Declara una variable full_name y asígnale un valor.
+full_name = first_name + ' '+ last_name
+print('Nombre completo:',full_name)
+
+# 4. Declara una variable country y asígnale un valor.
+country = 'Mexico'
+print('Pais:',country)
+
+# 5. Declara una variable city y asígnale un valor.
+city = 'Merida'
+print('Ciudad:',city)
+
+# 6. Declara una variable age y asígnale un valor.
+age = 28
+print('Edad:',age)
+
+# 7. Declara una variable year y asígnale un valor.
+year = 2026
+print('Año:',year)
+
+# 8. Declara una variable is_married y asígnale un valor.
+is_married = False
+print('Casado:',is_married)
+
+# 9. Declara una variable is_true y asígnale un valor.
+is_true = True
+print('Es verdad:',is_true)
+
+# 10. Declara una variable is_light_on y asígnale un valor.
+is_light_on = True
+print('Luz encendida:',is_light_on)
+
+# 11. Declara múltiples variables en una sola línea.
+first_name, last_name, country, age, is_married = 'Natt', 'Arrazola', 'Mexico', 18, True
+
+print(first_name, last_name, country, age, is_married)
+
