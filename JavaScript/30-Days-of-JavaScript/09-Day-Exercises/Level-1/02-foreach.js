115cc573d6e327beacd0a6e1bff52bc92265bc95
Nat Arrazola <NatyArrazola13@gmail.com>
2026-09-07 14:07:20 -0600

    Add Day 9 Exercises of Level 1 exercises 2 of JavaScript

---
+++ JavaScript/30-Days-of-JavaScript/09-Day-Exercises/Level-1/02-foreach.js
@@ -0,0 +1,23 @@
+// 3. Utiliza forEach para mostrar con console.log cada país del array de países.
+console.log("========== Ejercicio 3 ==========");
+const countries = ["Finland", "Sweden", "Denmark", "Norway", "IceLand"];
+
+countries.forEach((country) => {
+  console.log(country);
+});
+
+// 4. Utiliza forEach para mostrar con console.log cada nombre del array de nombres.
+console.log("========== Ejercicio 4 ==========");
+const names = ["Asabeneh", "Mathias", "Elias", "Brook"];
+
+names.forEach((name) => {
+  console.log(name);
+});
+
+// 5. Utiliza forEach para mostrar con console.log cada número del array de números.
+console.log("========== Ejercicio 5 ==========");
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+numbers.forEach((num) => {
+  console.log(num);
+});
